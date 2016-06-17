fec017b51655996e5cb26f378a5e81392f26807f
xc145214 <xc145214@gmail.com>
2016-06-17 16:02:59 +0800

    等待线程终结

---
+++ src/main/java/com/xc/java7concurrent/chapte1/FileClock.java
@@ -0,0 +1,55 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.xc.java7concurrent.chapte1;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程的休眠与恢复。
+ *
+ * @author xiachuan at 2016/6/17 15:37。
+ */
+
+public class FileClock implements Runnable {
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%s\n", new Date());
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.printf("The FileClock has been interrupted");
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        FileClock fileClock = new FileClock();
+        Thread thread = new Thread(fileClock);
+        thread.start();
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+    }
+}
+
---
+++ src/main/java/com/xc/java7concurrent/chapte1/NetWorkConnectionsLoader.java
@@ -0,0 +1,39 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.xc.java7concurrent.chapte1;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *  XXXXXXXXXXXXXXXXXXXXX
+ *
+ *  @author xiachuan at 2016/6/17 15:56。
+ */
+
+public class NetWorkConnectionsLoader implements Runnable {
+    @Override
+    public void run() {
+        System.out.printf("Beginning network connection:%s\n",new Date());
+
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Network connection has finished");
+    }
+}
+
