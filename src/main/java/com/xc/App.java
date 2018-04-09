73ae01558a15d13e5d883c7ad7571dadd23098d3
夏川 <xiachuan@my089.net>
2018-04-09 13:11:16 +0800

    add files

--- src/main/java/com/xc/App.java
+++ src/main/java/com/xc/App.java
@@ -1,22 +1,84 @@
 package com.xc;
 
-import org.slf4j.LoggerFactory;
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        File file = new File("E:\\DOC\\sql_slow.xlsx");
+
+        readNewExcel(file);
+
+    }
 
 
-        org.slf4j.Logger LOG = LoggerFactory.getLogger(App.class);
+    public static void readOldExcel(File xlsFile) throws IOException {
+        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(new FileInputStream(xlsFile));
+        HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(0);
+        Set<String> set = new HashSet<String>();
+        if (null != hssfSheet) {
+            for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
+                Row row = hssfSheet.getRow(rowNum);
 
+                if(null == row){
+                    continue;
+                }
 
-//        for (int i = 0; i < 10000; i++) {
-//            LOG.error("Info log [" + i + "].");
-//            Thread.sleep(500);
-//        }
+                if(row.getCell(0)==null || row.getCell(0).toString().equals("") || row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_BLANK){
+                    continue;
+                }
 
-        System.out.println(Integer.valueOf(1).equals(1));
+                String str = StringUtils.trim(row.getCell(10).getStringCellValue());
+                set.add(str.substring(str.lastIndexOf("*/") + 2));
+            }
+        }
+
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+
+    public static void readNewExcel(File xlsFile) throws IOException {
+        XSSFWorkbook xssfWorkbook  = new XSSFWorkbook(new FileInputStream(xlsFile));
+        XSSFSheet xssfSheet = xssfWorkbook .getSheetAt(0);
+        Set<String> set = new HashSet<String>();
+        if (null != xssfSheet) {
+            for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+                XSSFRow row = xssfSheet.getRow(rowNum);
+
+                if(null == row){
+                    continue;
+                }
+
+                if(row.getCell(0)==null || row.getCell(0).toString().equals("") || row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_BLANK){
+                    continue;
+                }
+
+                String str = StringUtils.trim(row.getCell(10).getStringCellValue());
+                set.add(str.substring(str.lastIndexOf("*/") + 2));
+            }
+        }
+
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 }
